76c76b3d75d8f65810d824025f5355da2cef4f56
Arman Carter <rman2534@gmail.com>
2014-04-14 18:21:36 -0400

    Implemented player jumping and removed rotation
    Player rotation has some unintended consequences (it can interfere with
    raycasting, etc…).

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,22 +3,39 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	
+	public int JumpHeight = 6;			
+	public bool isGrounded = false;
+	public Transform groundedEnd;
+
 	// Update is called once per frame
 	void Update () {
-
 		Movement();
+		Raycasting ();
 	}
 
+	// Show line used for raycasting and use raycasting to determine if player isGrounded
+	void Raycasting () {
+		Debug.DrawLine(this.transform.position, groundedEnd.position, Color.green);
+		isGrounded = Physics2D.Linecast(this.transform.position, groundedEnd.position, 1 << LayerMask.NameToLayer("Ground"));
+	}
+
+	// Handle player movement
 	void Movement() {
 
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.Translate (Vector2.right * 4f * Time.deltaTime);
-			transform.eulerAngles = new Vector3(0,0,-10);
+		//	transform.eulerAngles = new Vector3(0,0,-10);
 		}
 
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.Translate (-Vector2.right * 4f * Time.deltaTime);
-			transform.eulerAngles = new Vector3(0,0,10);
+		//	transform.eulerAngles = new Vector3(0,0,10);
 		}
+	
+		if (Input.GetKeyDown (KeyCode.UpArrow) && isGrounded == true) {
+			rigidbody2D.velocity = new Vector2 (0, JumpHeight);
+		}
+
 	}
+	
 }
