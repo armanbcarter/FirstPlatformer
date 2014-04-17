be8c2faebd014ad61c12cee923b170ea25ac1aa2
Arman Carter <rman2534@gmail.com>
2014-04-17 01:00:44 -0400

    Updated the player model and added animation
    Increased the size of the robot model and changed it so the default
    model faces right.  Added an idle animation and updated the moving and
    jumping code.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,49 +3,53 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	
-	public int JumpHeight = 6;			
+	float jumpForce = 300f;			
+	float maxSpeed = 5f;
+	bool facingRight = true;
 	public bool isGrounded = false;
 	public Transform groundedEnd;
 
-	// Update is called once per frame
-	void Update () {
-		Movement();
+	// Handle most physics in the game
+	void FixedUpdate ()
+	{
+		LeftRightMovement();
 		Raycasting ();
 	}
 
+	// Handle player input
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow) && isGrounded == true) 
+		{
+			rigidbody2D.AddForce (new Vector2 (0, jumpForce));
+		}
+	}
+
 	// Show line used for raycasting and use raycasting to determine if player isGrounded
-	void Raycasting () {
+	void Raycasting () 
+	{
 		Debug.DrawLine(this.transform.position, groundedEnd.position, Color.green);
 		isGrounded = Physics2D.Linecast(this.transform.position, groundedEnd.position, 1 << LayerMask.NameToLayer("Ground"));
 	}
 
-	// Handle player movement
-	void Movement() {
+	// Handle left/right player movement
+	void LeftRightMovement() 
+	{
+		float move = Input.GetAxis ("Horizontal");
 
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			if(isGrounded) {
-				transform.Translate (Vector2.right * 4f * Time.deltaTime);
-			}
-			else {
-				transform.Translate (Vector2.right * 2f * Time.deltaTime);
-			}		
-		//	transform.eulerAngles = new Vector3(0,0,-10);
-		}
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
 
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			if(isGrounded) {
-				transform.Translate (-Vector2.right * 4f * Time.deltaTime);
-			}
-			else {
-				transform.Translate (-Vector2.right * 2f * Time.deltaTime);
-			}
-		//	transform.eulerAngles = new Vector3(0,0,10);
-		}
-	
-		if (Input.GetKeyDown (KeyCode.UpArrow) && isGrounded == true) {
-			rigidbody2D.velocity = new Vector2 (0, JumpHeight);
-		}
-
+		if (move > 0 &&!facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
 	}
 	
+	void Flip () 
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
 }
