bb609dbeaa94175310004e1ee5c0497a4aca47ce
Arman Carter <rman2534@gmail.com>
2014-04-20 23:55:30 -0400

    Updated the camera script to remove bugs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	Vector3 bufferXStart, bufferXEnd;
-	Vector3 initialTarget;
+	Vector3 initialTarget, finalTarget;
 	Vector3 destination;
 	Vector3 velocity = Vector3.zero;
 	float smoothTime = .15f;
@@ -18,6 +18,7 @@ public class CameraController : MonoBehaviour {
 		initialTarget = target.position + new Vector3 (7.2f, 2.8f, -10);
 		bufferXStart = target.position + new Vector3 (10, 1, 0);				 	// Sets the position of the x-direction start buffer
 		bufferXEnd = target.position + new Vector3 (25, 1, 0);						// Sets the position of the x-direction end buffer						
+		finalTarget = target.position + new Vector3 (24.5f, 2.8f, -10);
 	}
 	
 	// Update is called once per frame
@@ -28,12 +29,24 @@ public class CameraController : MonoBehaviour {
 		// If the robot stays within the x-direction buffer, the camera follows the player
 		if (target.position.x >= bufferXStart.x && target.position.x <= bufferXEnd.x)		
 		{
-			destination = target.position + new Vector3 (0, 2.8f, -10);
-			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, smoothTime); 
+			if (GameObject.Find("RobotDefault").GetComponent<PlayerController>().isGrounded)
+			{
+				destination = target.position + new Vector3 (0, 2.8f, -10);
+				transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, smoothTime); 
+			}
+			else
+			{
+				destination = target.position + new Vector3 (0, 2f, -10);
+				transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, smoothTime);	
+			}
 		}
 		else if (target.position.x <= bufferXStart.x)
 		{
-			transform.position = Vector3.SmoothDamp(transform.position, initialTarget, ref velocity, smoothTime);
+			transform.position = Vector3.SmoothDamp(transform.position, initialTarget, ref velocity, smoothTime);	
+		}
+		else if (target.position.x >= bufferXEnd.x)
+		{
+			transform.position = Vector3.SmoothDamp(transform.position, finalTarget, ref velocity, smoothTime);	
 		}
 	}
 }
